2b3d3e8a57c3e7f022e1cfc2d7117db0780a06a8
Om Dahale <om.g.dahale@gmail.com>
2022-08-31 17:26:44 +0530

    add is_hod, is_teacher and is_student to user modeladmin

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import *
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets
+    fieldsets[0][1]['fields'] += ('is_hod', 'is_teacher', 'is_student')
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Student)
 admin.site.register(StudentRequest)
